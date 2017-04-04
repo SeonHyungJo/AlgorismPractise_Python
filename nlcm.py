5e56540108f2c233c41b20e3ad9d8a8b1b028247
SeonHyungJo <senjing9379@gmail.com>
2017-04-04 22:24:23 +0900

    solve the problem

--- nlcm.py
+++ nlcm.py
@@ -5,25 +5,29 @@ n개의 수의 최소공배수는 n 개의 수들의 배수 중 공통이 되는
 nlcm 함수를 통해 n개의 숫자가 입력되었을 때, 최소공배수를 반환해 주세요.
 예를들어 [2,6,8,14] 가 입력된다면 168을 반환해 주면 됩니다.
 """
-#미해결
+#해결
 def nlcm(num):
-    n1 = 1
-    count = 0
-    lista = []
+    n1 = num.pop()
     for i in range(len(num)):
-        for j in range(len(num)):
-            a = num[i]
-            b = num[j]
-            
-            while b:
-            	a, b = b, a % b
-            n1 = a * n1
-            for k in range(len(num)):
-                if num[k] % a == 0:
-                    num.remove(num[k])
-                    num.insert(k-1, num[k]//a)
-
-        print(num)
+        n2 = num.pop()
+        a, b = n1, n2
+        while b:
+            a, b = b, a % b
+        n1 = n1 * n2 // a
     return n1
+
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(nlcm([1,6,8,14]));
+print(nlcm([81, 52, 15, 74, 28, 25, 60, 34, 14, 37]));
+
+"""
+다른풀이(gcd함수가 있구나)
+from fractions import gcd
+
+
+def nlcm(num):
+    answer = num[0]
+    for n in num:
+        answer = n * answer / gcd(n, answer)
+
+    return answer
+"""
